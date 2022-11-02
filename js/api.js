743276ad957e0bed57cffc5c4cc156a1db635f9b
Marcos Victor da Silva Barbosa <marcosvictorsb@gmail.com>
2022-11-02 15:58:13 -0300

    api

---
+++ js/api.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('#sheetdb-form')
+const btn = document.querySelector('.sheetdb-form-btn')
+const input = document.querySelector('#sheetdb-form-input')
+const inputDate = document.querySelector('#sheetdb-form-input-date');
+/*
+  Primeiro Formulario
+*/
+
+function changeContentBtn(value) {
+  btn.textContent = value;
+}
+
+function clearInput() {
+  input.value = "";
+}
+
+function fetchSheetDB() {
+  fetch(form.action, {
+    method: 'POST',
+    body: new FormData(document.getElementById('sheetdb-form'))
+  }).then(response => response.json());
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  changeContentBtn('Enviando ...')
+  inputDate.value = new Date();
+  setTimeout(function () {
+    fetchSheetDB()
+    changeContentBtn('Enviar')
+    clearInput();
+  }, 2000);
+})
+
+
+/*
+  Secondo Formulario
+*/
+
+const formSecond = document.querySelector('#sheetdb-form-2')
+const btnSecond = document.querySelector('.sheetdb-form-btn-second')
+const inputSecond = document.querySelector('#sheetdb-form-input-second')
+const inputDateSecond = document.querySelector('#sheetdb-form-input-date-second');
+
+function changeContentBtn(value) {
+  btnSecond.textContent = value;
+}
+
+function clearInput() {
+  inputSecond.value = "";
+}
+
+function fetchSheetDB() {
+  fetch(formSecond.action, {
+    method: 'POST',
+    body: new FormData(document.getElementById('sheetdb-form-2'))
+  }).then(response => response.json());
+}
+
+formSecond.addEventListener("submit", function (e) {
+  e.preventDefault();
+  changeContentBtn('Enviando ...')
+  inputDateSecond.value = new Date();
+  setTimeout(function () {
+    fetchSheetDB()
+    changeContentBtn('Enviar')
+    clearInput();
+  }, 2000);
+})
